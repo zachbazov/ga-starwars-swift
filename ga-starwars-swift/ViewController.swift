1e8c6d6119ac49ce3d5df19dd4ff2cc11afa1f97
zachbazov <zachbazov@gmail.com>
2023-10-01 13:52:01 +0300

    Initial Commit

---
+++ ga-starwars-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ga-starwars-swift
+//
+//  Created by Developer on 01/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
